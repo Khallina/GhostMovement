4e2106e6939432430204ea88ca9d414f60c6492b
Safwan Muntasir <97055159+smuntasi@users.noreply.github.com>
2024-02-15 16:05:24 -0800

    added draw func to dummy spider

--- DummySpider.java
+++ DummySpider.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class DummySpider {
     private int posX;
     private int posY;
@@ -14,5 +16,17 @@ public class DummySpider {
     public int getPosY() {
         return posY;
     }
+
+
+    public void draw(Graphics g){
+
+
+        int x = this.getPosX();
+        int y = this.getPosY();
+        g.setColor(Color.BLACK);
+        g.fillRect(x,y,5,5);
+    }
+
+
 
 }
