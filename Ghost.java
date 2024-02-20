e5ddb9e44d7020dfe8f93dc4e7457d246d9583e5
Katie Hallinan <102558764+Khallina@users.noreply.github.com>
2024-02-19 19:49:49 -0800

    Final

--- Ghost.java
+++ Ghost.java
@@ -12,22 +12,6 @@ public class Ghost extends Observable implements Drawable {
     }
 
     public void draw(Graphics g) {
-        /*
-        if (Math.random()>0.5)
-            if (Math.random()>0.5)
-                positionX += 20;
-            else
-                positionX -= 20;
-        else
-            if (Math.random()>0.5)
-                positionY += 20;
-            else
-                positionY -= 20;
-
-         */
-        // positionX = 25;
-        //  positionY = 25;
-
         if (positionX > 400) positionX = 400;
         if (positionY > 400) positionY = 400;
         if (positionX < 0) positionX = 0;
